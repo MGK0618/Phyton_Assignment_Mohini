13ec41b9a7c87a267a8108e39752f5b50c3e3373
MGK0618 <mjadhav9704@gmail.com>
2026-02-10 16:07:48 +0530

    assignment commit

---
+++ Task 1 _A1.py
@@ -0,0 +1,21 @@
+# Task 1: Perform Basic Mathematical Operations
+#Step 1 Take the input from user
+num1 = int(input("Enter First number: "))
+num2 = int(input("Enter Second number: "))
+
+#Step 2 Perform Operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+#Step 3 Display Results
+print("\nResults:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+
+
+
